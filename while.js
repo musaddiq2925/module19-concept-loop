32df91eec4c95b433074f2fdcc4694c43a069278
nourinCT <nourin.ct@gmail.com>
2025-03-16 02:03:14 +0600

    file

--- while.js
+++ while.js
@@ -1,8 +1,22 @@
 //---- While Loop ----//
-let number = 0; // loop variable
-while(number < 10){
-    console.log('number', number);
-    // number = number + 1;
-    // number += 1;
-    number++;
+// let number = 0; // loop variable
+// while(number < 10){
+//     console.log('number', number);
+//     // number = number + 1;
+//     // number += 1;
+//     number++;
+// }
+
+//
+let num = 0;
+while (num < 10) {
+    console.log('number:', num);
+    num++;
 }
+
+//
+// let n = 0;
+// while (n <= 10) {
+//     console.log('n', n);
+//     n--;
+// }
